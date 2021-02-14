1d34577b4f10a96ee646f793b4e012bb274ef89e
Alex Vale <alexjvale21@gmail.com>
2021-02-14 11:30:55 +0000

    .

--- uniques/vimeo-embed.js
+++ uniques/vimeo-embed.js
@@ -1,1 +1,15 @@
-document.getElementById("vimeo-embed").classList.add("large");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!************************************************!*\
+  !*** ../../../demo/src/uniques/vimeo-embed.ts ***!
+  \************************************************/
+eval("document.getElementById('vimeo-embed').classList.add('large');\n\n\n//# sourceURL=webpack:///../../../demo/src/uniques/vimeo-embed.ts?");
+/******/ })()
+;
